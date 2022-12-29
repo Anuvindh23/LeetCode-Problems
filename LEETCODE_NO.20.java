cca96f62b67d3a5092588cd8cc708ab6f07dcc8a
Anuvindh23 <116289179+Anuvindh23@users.noreply.github.com>
2022-12-29 11:22:22 +0530

    Add files via upload

---
+++ LEETCODE_NO.20.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char i:s.toCharArray()){
+            if(i=='('||i=='{'||i=='['){
+                st.push(i);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char ch=st.pop();
+                if((i==')'&&ch=='(')||(i=='}'&&ch=='{')||(i==']'&&ch=='[')){
+
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
